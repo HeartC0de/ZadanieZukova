da5a790d01ed92c8ba8a97d25fe568b7dad037c4
Павел Криволапов <pavel.krivo@yandex.ru>
2024-05-29 16:29:00 +0300

    Добавьте файлы проекта.

---
+++ ZadanieZukova/Interval.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZadanieZukova
+{
+    internal class Interval
+    {
+
+        public string StartTime { get; set; } = string.Empty;
+        public string EndTime { get; set; } = string.Empty;
+
+        public Interval(string start, string end)
+        {
+            StartTime = start;
+            EndTime = end;
+        }
+
+        public void freeSpoons()
+        {
+            
+        }
+    }
+}
---
+++ ZadanieZukova/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+using ZadanieZukova;
+
+List<Interval> Hold_intervals = new List<Interval>();
+
+Hold_intervals.Add(new Interval("12:30", "13:00"));
+Hold_intervals.Add(new Interval("15:50", "16:20"));
+Hold_intervals.Add(new Interval("17:30", "18:00"));
+Hold_intervals.Add(new Interval("18:05", "19:25"));
+Hold_intervals.Add(new Interval("19:30", "20:00"));
+
+for (int i = 0; i < Hold_intervals.Count-1; i++)
+{
+    string[] timeEnd;
+    string[] timeStr;
+    Interval intervalE = Hold_intervals[i];
+    Interval intervalS = Hold_intervals[i+1];
+    int End;
+    int Start;
+    int free = 0;
+    timeEnd = intervalE.EndTime.Split(':');
+    End = int.Parse(timeEnd[0])*60 + int.Parse(timeEnd[1]);
+    timeStr = intervalS.StartTime.Split(':');
+    Start = int.Parse(timeStr[0])* 60 + int.Parse(timeStr[1]);
+    for (int j = End+30; j < Start; j += 30)
+    {
+        Console.WriteLine(intervalE.StartTime);
+    }
+
+}
+
+
+List<Interval> Free_intervals = new List<Interval>();
